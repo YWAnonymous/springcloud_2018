d278ae895ff3065e10147038d7d98ed3e6c8fd90
YWAnonymous <1274315645@qq.com>
2020-11-04 14:20:19 +0800

    config

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/zhou/springcloud/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.zhou.springcloud.service;
+
+import java.util.List;
+
+import com.zhou.springcloud.entities.Dept;
+
+public interface DeptService
+{
+	public boolean add(Dept dept);
+
+	public Dept get(Long id);
+
+	public List<Dept> list();
+}
---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/zhou/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.zhou.springcloud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zhou.springcloud.dao.DeptDao;
+import com.zhou.springcloud.entities.Dept;
+import com.zhou.springcloud.service.DeptService;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
